26f8c0fbaa99b7d1822fb65724b052641f0dcc4b
ojas thakur <ojasthakurx@gmail.com>
2022-03-17 15:05:52 +0530

    changed isUri to isWebUri to check for http/https

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -22,7 +22,7 @@ const createUrl = async (req, res) => {
 
     //check if long url is valid using the validUrl.isUri method
 
-    if (validUrl.isUri(original_url)) {
+    if (validUrl.isWebUri(original_url)) {
         try {
             let url = await Url.findOne({
                 original_url
